8a80ae1a9b9fb64e460ebc91a061134ac2348d58
Ali Shandi <ashand01@seattlecentral.edu>
2017-06-15 16:00:58 -0700

    Create apache_django_install.sh

--- automation/apache_django_install.sh
+++ automation/apache_django_install.sh
@@ -114,3 +114,38 @@ echo "from django.contrib.auth.models import User; User.objects.create_superuser
 
 #start djanngo server in the background <-- use fg to bring the process to the foreground and ctrl-c to quit
 python manage.py runserver 0.0.0.0:8000&
+
+
+
+#this is my montoring and tredning script
+
+    myusername="ashand01"                         # set this to your username
+    mynagiosserver="nagios-a"                     # set this to your nagios server name
+    mycactiserver="cacti-server"                      # set this to your cacti server
+    myreposerver="yumrepo-server"                       # set this to your repo server
+    mynagiosserverip="146.148.42.252"                   # set this to the ip address of your nagios server
+
+    generate_config.sh $1 $2              # code I gave you in a previous assignment that generates a nagios config
+
+    gcloud compute copy-files $1.cfg $myusername@$mynagiosserver:/etc/nagios/conf.d
+
+                                      # note: I had to add user my gcloud user to group nagios using usermod -a -G nagios 
+    $myusername on my nagios server in order to make this work.
+                                      # I also had to chmod 770 /etc/nagios/conf.d
+
+    configstatus=$( \
+    gcloud compute ssh $myusername@$mynagiosserver \
+    "sudo /usr/sbin/nagios -v /etc/nagios/nagios.cfg" \
+    | grep "Things look okay - No serious problems" \
+    )
+
+    if [[ $configstatus ]]; then 
+    gcloud compute ssh $myusername@$mynagiosserver "sudo systemctl restart nagios"
+    echo "$1 has been added to nagios."
+    else
+    echo "There was a problem with the nagios config, please log into $mynagiosserver and run /usr/sbin/nagios -v 
+    /etc/nagios/nagios.cfg to figure out where the problem is"; 
+    exit 1;
+    fi
+
+    # (I'll throw the cacti code in here in a bit)
